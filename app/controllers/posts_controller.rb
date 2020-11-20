dc11bb0e36a3eb2754e6095a68852794d18441fb
makky-y <makky__y@hotmail.com>
2020-11-20 23:53:21 +0900

    controller編集

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,3 +5,19 @@ class PostsController < ApplicationController
   end
   
 end
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+  has_one :card, dependent: :destroy
+  has_many :delivery_addresses, dependent: :destroy
+  accepts_nested_attributes_for :delivery_addresses
+  has_one :user_profile, dependent: :destroy
+  accepts_nested_attributes_for :user_profile
+
+  validates :nickname, presence: true
+  validates :email, presence: true, uniqueness: true
+  validates :encrypted_password, presence: true
+
+end
